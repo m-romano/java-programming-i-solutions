1257ea7f32a91c63b715576b0d8ca3a6610451de
Marco Romano <mromanode@gmail.com>
2024-04-02 23:09:43 +0200

    feat(ExerciseManagementTest): Ignore my shenanigans; tests weren't add at the time of the commit.

--- part06/part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06/part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,46 @@
+import static org.junit.Assert.*;
 
-
+import org.junit.Before;
+import org.junit.Test;
 
 public class ExerciseManagementTest {
 
+    private ExerciseManagement management;
+
+    @Before
+    public void initialize() {
+        management = new ExerciseManagement();
+    }
+
+    @Test
+    public void exerciseListEmptyAtBeginning() {
+        assertEquals(0, management.exerciseList().size());
+    }
+
+    @Test
+    public void addingExerciseGrowsListByOne() {
+        management.add("Write a test");
+        assertEquals(1, management.exerciseList().size());
+    }
+    @Test
+    public void addedExerciseIsInList() {
+        management.add("Write a test");
+        assertTrue(management.exerciseList().contains("Write a test"));
+    }
+
+    // Own test unit
+    @Test
+    public void givenExercise_whenDeletingExistingExercise_thenItIsRemovedFromTheList() {
+        ExerciseManagement management = new ExerciseManagement();
+        management.add("New exercise");
+        management.add("Another exercise");
+
+        assertTrue(management.exerciseList().contains("New exercise"));
+        int sizeBeforeDeletion = management.exerciseList().size();
+
+        management.remove("New exercise");
+
+        assertEquals(sizeBeforeDeletion - 1, management.exerciseList().size());
+        assertFalse(management.exerciseList().contains("New exercise"));
+    }
 }
