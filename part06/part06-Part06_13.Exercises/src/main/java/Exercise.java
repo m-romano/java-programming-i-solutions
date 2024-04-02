a08f8e63e9b3f73bacede027c98c3ff28c9fe5f4
Marco Romano <mromanode@gmail.com>
2024-04-02 22:50:12 +0200

    feat: add solution for part06-Part06_13.Exercises.
    Pretty cool exercise. I wanted to add a userInterface class, maybe
    another time. This solution also include the optional test unit to
    remove an exercise from the arraylist. Kudos to get this far!

---
+++ part06/part06-Part06_13.Exercises/src/main/java/Exercise.java
@@ -0,0 +1,37 @@
+import java.util.Objects;
+
+public class Exercise {
+
+    private String name;
+    private boolean completed;
+
+    public Exercise(String name) {
+        this.name = name;
+        this.completed = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Exercise exercise = (Exercise) o;
+        return Objects.equals(name, exercise.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
